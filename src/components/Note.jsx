8e9d7ea53bc2db03306da68fe9a7ac7a853b0394
Fraasi <doofassi@gmail.com>
2018-04-23 01:05:22 +0300

    work on notebook + trying to fix resize warning

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,12 +3,6 @@ import showdown from 'showdown';
 
 showdown.setFlavor('github')
 const converter = new showdown.Converter()
-const text = `# hello, markdown!
-- [ ] jhjhjh  
-* kjkjkj
-
-background-color: #35363A;`
-
 // console.log(showdown.getOptions())
 
 export default class Note extends Component {
@@ -16,12 +10,17 @@ export default class Note extends Component {
 		super()
 		this.state = {
 			dateCreated: Date.now(),
-			rawText: text,
+			rawText: `# hello, markdown!
+			- [ ] jhjhjh  
+			* kjkjkj
+			background-color: #35363A;`,
 			editMode: false
 		}
 		this.changeEditMode = this.changeEditMode.bind(this)
 		this.saveEdit = this.saveEdit.bind(this)
 		this.barsClick = this.barsClick.bind(this)
+		this.addNewNote = this.addNewNote.bind(this)
+		this.deleteNote = this.deleteNote.bind(this)
 	}
 	componentDidMount() {
 	}
@@ -48,24 +47,59 @@ export default class Note extends Component {
 		})
 	}
 
+	addNewNote() {
+		const infoText = '<!-- Untitled, first word on first line will be the title --> \n* click editbutton or press ctrl+enter to save and close edit box\n'
+
+		if (this.state.editMode === true) {
+			// eslint-disable-next-line
+			document.querySelector('.note-editbox').value = infoText
+		}
+
+		this.setState({
+			dateCreated: Date.now(),
+			rawText: infoText,
+			editMode: true
+		})
+	}
+
+	deleteNote() {
+		// eslint-disable-next-line
+		const conf = confirm('Are you sure you want to delete this note?')
+		console.log(conf, this)
+		// const obj = obj.pop(this.note)
+		let pre;
+		if (conf) pre = '- [x] '
+		else pre = '- [ ] '
+		this.setState({
+			rawText: pre + conf,
+			editMode: false
+		})
+	}
+
 
 	render() {
 		return (
 
 			<div id="note-wrapper">
-				<h3 className="note-title">{`${this.state.rawText.match(/\w+/)} - ${new Date(this.state.dateCreated).toLocaleString()}`}
-				<img className="fa-icon bars" src="./assets/img/bars.svg" alt="bars.svg" style={{ verticalAlign: 'baseline', float: 'left' }} onClick={this.barsClick} />
-				<img className="fa-icon edit" src="./assets/img/edit.svg" alt="edit.svg" style={{ verticalAlign: '10%', float: 'right' }} onClick={this.changeEditMode} />
-				</h3>
+				<h3 className="note-title">{`${this.state.rawText.match(/\w\w+/)} - ${new Date(this.state.dateCreated).toLocaleString('de', { hour12: false }).replace(/\./g, '/')}`}
 
+					<img className="fa-icon plus-square" src="./assets/img/plus-square.svg" alt="plus-square.svg" style={{ verticalAlign: 'baseline', float: 'left' }} onClick={this.addNewNote} />
+
+					<img className="fa-icon bars" src="./assets/img/bars.svg" alt="bars.svg" style={{ verticalAlign: 'baseline', float: 'left' }} onClick={this.barsClick} />
+
+					<img className="fa-icon minus-square" src="./assets/img/minus-square.svg" alt="minus-square.svg" style={{ verticalAlign: '10%', float: 'right' }} onClick={this.deleteNote} />
+
+					<img className="fa-icon edit" src="./assets/img/edit.svg" alt="edit.svg" style={{ verticalAlign: '10%', float: 'right' }} onClick={this.changeEditMode} />
+				</h3>
 
 				{
 					this.state.editMode === false &&
+					// eslint-disable-next-line
 					<div className="note-text" dangerouslySetInnerHTML={{ __html: converter.makeHtml(this.state.rawText) }} />
 				}
 				{
 					this.state.editMode === true &&
-					<textarea className="note-editbox" defaultValue={this.state.rawText} onChange={this.saveEdit} />
+					<textarea className="note-editbox" defaultValue={this.state.rawText} onChange={this.saveEdit} autoFocus />
 				}
 
 			</div>
