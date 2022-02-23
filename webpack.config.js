93f11f87c629144525263d807532e206b8e0b0d9
fraasi <doofassi@gmail.com>
2022-02-23 22:12:16 +0200

    remove @babel/polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   watch: ENV === 'development',
   devtool: ENV === 'development' ? 'eval-source-map' : 'source-map',
   target: 'electron-renderer',
-  entry: ['./@babel/polyfill', './app/renderer.jsx'],
+  entry: ['./app/renderer.jsx'],
   output: {
     path: `${__dirname}/app/build`,
     publicPath: 'build/',
@@ -32,16 +32,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-
-              // hmr: process.env.NODE_ENV === 'development',
-            },
-          },
-          'css-loader',
-        ],
+        use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
